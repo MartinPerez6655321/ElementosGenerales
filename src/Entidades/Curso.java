e047098999a199e3630b77f9358bd4d9615b06a8
MartinPerez6655321 <martin.nahuel.perez@hotmail.com>
2018-12-27 08:38:08 -0300

    Commit creación de la clase curso

--- src/Entidades/Curso.java
+++ src/Entidades/Curso.java
@@ -1,5 +1,43 @@
 package Entidades;
 
 public class Curso {
+	
+	//Atributos de un Curso
+	
+	private Integer idCurso;
+	private String anioCurso;
+	private String turnoCurso;
+	
+	//Constructor de Curso
+	
+	public Curso (Integer id, String anio, String turno) {
+		idCurso = id;
+		anioCurso = anio;
+		turnoCurso = turno;
+	}
 
+	//Getters & Setters de Curso
+	
+	public String getAnioCurso() {
+		return anioCurso;
+	}
+
+	public void setAnioCurso(String anioCurso) {
+		this.anioCurso = anioCurso;
+	}
+
+	public String getTurnoCurso() {
+		return turnoCurso;
+	}
+
+	public void setTurnoCurso(String turnoCurso) {
+		this.turnoCurso = turnoCurso;
+	}
+
+	public Integer getIdCurso() {
+		return idCurso;
+	}
+
+	
+	
 }
